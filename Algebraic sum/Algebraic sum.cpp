2d3fc07fdd4ba7c4250b83557a1d2675a910382a
Максим <@DESKTOP-9JOEI7F>
2023-03-19 23:57:16 +0200

    Добавьте файлы проекта.

---
+++ Algebraic sum/Algebraic sum.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+double algebraic_sum()
+{
+	double z, b, a, betta, summa;
+	std::cout << "Enter value(z,b,a): \n" << "Z = ";
+	std::cin >> z;
+	std::cout << "B = ";
+	std::cin >> b;
+	std::cout << "A = ";
+	std::cin >> a;
+	std::cout << "Betta = ";
+	std::cin >> betta;
+	summa = (z * z * z) - b + (a * a) / (tan(betta) * tan(betta));
+
+	
+	return summa;
+}
+
+
+int main()
+{
+	std::cout << "find by the formula - " << "Y = X1 + X2 + … + Xn \t" << "X = Z^3 - B + A^2 / tg^2\n";
+	std::vector<double> ixi;
+	int x;
+	double y = 0;
+	std::cout << "Enter x - ";
+	std::cin >> x;
+	for (x != 0; x--;)
+	{
+		ixi.push_back(algebraic_sum());
+	}
+	for (int i = 0; i < ixi.size(); i++)
+	{
+		y += ixi[i];
+	}
+	std::cout <<"Y = " << y;
+}
